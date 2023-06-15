b7324f65f441ce16f8622bac512a27aa2beaec6a
farhaddanish <farhaddanish22@gmail.com>
2023-06-15 14:15:34 -0400

    API integrated with React and db

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
 import React from "react";
 import BookCreate from "./components/BookCreate";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookList from "./components/BookList";
+import axio from "axios";
+import axios from "axios";
 
 function App() {
     const [books, setbooks] = useState([]);
     const random_id = Math.floor(Math.random() * 100);
-    const createBook = (title) => {
-        const updated_books = [
-            ...books,
-            {
-                id: random_id,
-                title,
-            },
-        ];
+
+    const fetchBooks = async () => {
+        const response = await axios.get("http://localhost:3002/books", {});
+        setbooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const createBook = async (title) => {
+        const response = await axios.post("http://localhost:3002/books", {
+            title: title,
+        });
+
+        const updated_books = [...books, response.data];
 
         setbooks(updated_books);
     };
 
     const deleteBook = (id) => {
-        const updated_books = books.filter((value, index) => {
-            return value.id !== id;
-        });
+        const user_answer = prompt("Do you Want to delete this book yes/no");
+        if (user_answer === "yes") {
+            const response = axios.delete(`http://localhost:3002/books/${id}`);
 
-        setbooks(updated_books);
+            const updated_books = books.filter((value, index) => {
+                return value.id !== id;
+            });
+
+            setbooks(updated_books);
+        }
     };
 
-    const updateBook = (id, title) => {
+    const updateBook = async (id, title) => {
+        const response = await axios.put(`http://localhost:3002/books/${id}`, {
+            title: title,
+        });
+
         const updatebook = books.map((value, index) => {
             if (value.id === id) {
-                return { ...value, title };
+                return { ...value, ...response.data };
             }
 
             return value;
         });
 
-        setbooks( updatebook);
+        setbooks(updatebook);
     };
 
     const checkforBooks =
@@ -49,7 +68,6 @@ function App() {
             <h1>International Book Show</h1>
             {checkforBooks}
             <div>
-               
                 <BookList
                     books={books}
                     onDelete={deleteBook}
